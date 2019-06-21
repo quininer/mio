9a0e82668c61d0330f4c50497724531d228a31f5
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-06-21 03:55:32 +0000

    Deploy Mio API documentation

---
+++ doc/mio/event/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Event","A readiness event."],["Events","A collection of readiness events."],["Iter","[`Events`] iterator."]],"trait":[["Evented","A value that may be registered with [`Registry`]."]]});
---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["mio"] = {"name":"","dirs":[{"name":"event","dirs":[],"files":["event.rs","evented.rs","events.rs","mod.rs"]},{"name":"net","dirs":[],"files":["mod.rs","tcp.rs","udp.rs"]},{"name":"sys","dirs":[{"name":"unix","dirs":[],"files":["dlsym.rs","epoll.rs","eventedfd.rs","io.rs","mod.rs","tcp.rs","udp.rs","uio.rs","waker.rs"]}],"files":["mod.rs"]}],"files":["interests.rs","lib.rs","poll.rs","token.rs","waker.rs"]};
+createSourceSidebar();
